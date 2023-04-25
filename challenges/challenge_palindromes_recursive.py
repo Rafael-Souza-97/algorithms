ee65d743fbfa512b132ef6ae67e7ce7835919caf
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-25 16:00:05 -0300

    refactor: req 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,5 +1,4 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    print(word, low_index, high_index)
     if not word:
         return False
     if word[low_index] != word[high_index]:
