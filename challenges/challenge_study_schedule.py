3c34f837b0ba0e51e91f7a6ac8838b5a40c94202
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-25 15:18:01 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    students = 0
+    for period in permanence_period:
+        if not period[0] or not period[1] \
+                or type(period[0]) == str or type(period[1]) == str:
+            return None
+        if period[0] <= target_time <= period[1]:
+            students += 1
+    return students
