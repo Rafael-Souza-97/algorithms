ad52b56ce46f1d2cf6e4f7b0ee76c5a6961388e8
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-25 15:47:18 -0300

    req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "5")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(5, 6)
+
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("test", 4) == "tset"
